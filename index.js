7f41aa929ebf4fa21c764d473da3a48c9435a53a
Tracy Collins <tc@threeceemedia.com>
2020-02-27 09:05:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 function getFirstSelector(selector){
   console.log(selector)
-  console.log(document.querySelector(selector))
+  console.log(document.getElementById('app').querySelector(selector))
   return document.querySelector(selector)
 }
 
 function nestedTarget(){
-  
+  var elem = document.querySelectorAll.
+}
+
+function deepestChild() {
+
+}
+
+function increaseRankBy() {
+
 }
