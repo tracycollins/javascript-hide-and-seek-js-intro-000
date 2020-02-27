256bf02715e24b5b7311aa5894a5a5d88cb03c7e
Tracy Collins <tc@threeceemedia.com>
2020-02-27 09:16:36 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -2,7 +2,7 @@ describe('index', () => {
   describe('getFirstSelector(selector)', () => {
     it('returns the first element that matches the selector', () => {
       expect(getFirstSelector('div').id).to.equal('nested')
-      // expect(getFirstSelector('.ranked-list')).to.equal(document.querySelector('.ranked-list'))
+      expect(getFirstSelector('.ranked-list')).to.equal(document.querySelector('.ranked-list'))
     })
   })
 
